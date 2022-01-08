6e45b1bb31d596b9ef4ff9998a53565402f296ea
violettebunny <52981973+violettebunny@users.noreply.github.com>
2022-01-08 17:57:15 -0500

    added buttons

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,34 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Leon Web Designs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          <h1 className="text-blue-600 typewriter" href="https://nextjs.org">
-            Violet Leon
-          </h1>
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <div className=""></div>
+        <h1 className="text-6xl mb-5">
+          Hello, I'm{' '}
+          <span
+            className="text-6xl font-bold text-blue-600 typewriter"
+            href="https://nextjs.org"
+          >
+            Violet Leon.
+          </span>
+          <p className="mt-3">I'm a full-stack developer.</p>
         </h1>
 
-        <h2 className="text-2xl mt-10">Full-stack Web Developer</h2>
-        <h2 className="text-2xl mt-2">Full-stack Web Developer</h2>
-        <h2 className="text-2xl mt-2">Full-stack Web Developer</h2>
-        <h2 className="text-2xl mt-2">Full-stack Web Developer</h2>
-        <h2 className="text-2xl mt-2">Full-stack Web Developer</h2>
+        <div className="flex flex-row mt-20">
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-700">
+            About Me
+          </button>
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2">
+            My Projects
+          </button>
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-700">
+            View My Resume
+          </button>
+        </div>
       </main>
     </div>
   );
