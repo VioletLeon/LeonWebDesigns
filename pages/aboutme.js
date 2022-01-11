0e97dd6e53f307813a3888f30ed42294b862eb31
violettebunny <52981973+violettebunny@users.noreply.github.com>
2022-01-10 22:00:12 -0500

    general layout of about me is setup

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -30,30 +30,34 @@ export default function Home() {
 
       <Navbar />
 
-      <main className="flex flex-col justify-center flex-1 px-20 text-center min-w-full">
+      <main className="flex flex-col justify-center flex-1 px-40 text-center min-w-full">
         <div className="flex sm:flex-row flex-col justify-evenly">
-          <div className="flex flex-col bg-backgroundSecondary border-gray-900 hover:border-lightBlue border-4 max-w-sm align-center justify-center">
+          <div className="flex flex-col bg-backgroundSecondary border-gray-900 hover:border-lightBlue border-4 max-w-sm max-h-[750px] align-center justify-center p-5">
             <div className="">
               <h2 className="text-4xl mb-5 text-lightPurple">About me:</h2>
             </div>
             <div className="text-xl">
               <p className="">
                 I'm a recent bootgramp graduate from{' '}
-                <span className="text-lightPurple">Fullstack Academy</span> and
-                I'm interested in mobile development and front-end technologies.{' '}
+                <span className="text-lightPurple">Fullstack Academy,</span>{' '}
+                where I created projects in a team using the{' '}
+                <span className="text-lightPurple"> PERN stack</span> and{' '}
+                <span className="text-lightPurple">agile methodologies.</span>{' '}
+                I'm interested in mobile development and front-end technologies,
+                and I'm looking to land my first software engineering job.
               </p>
               <p></p>
               <p className="mt-10">
-                If you're interested in working with me please,{' '}
+                If you're interested in working with me please, <br />
                 <Link href="/contact">
                   <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg sm:text-lg text-sm sm:px-5 px-2 sm:py-2.5 py-1 text-center mr-2 mb-2 mt-5 transition-all duration-700">
-                    CONTACT ME
+                    Contact Me
                   </button>
                 </Link>
               </p>
             </div>
-            <div className="flex flex-row justify-end">
-              <div className="m-10 mb-20 max-w-[75px] max-h-[75px] text-lightPurple hover:text-lightBlue focus:ring-blue-300 dark:focus:ring-blue-800">
+            <div className="flex flex-row justify-evenly">
+              <div className="m-10 mb-20 max-w-[50px] max-h-[50px] text-lightPurple hover:text-lightBlue focus:ring-blue-300 dark:focus:ring-blue-800">
                 <Link href="https://github.com/VioletLeon">
                   <a target="_blank">
                     {' '}
@@ -62,7 +66,7 @@ export default function Home() {
                 </Link>
                 <span>Github</span>
               </div>
-              <div className="m-10 max-w-[75px] max-h-[75px] text-lightPurple hover:text-lightBlue">
+              <div className="m-10 max-w-[50px] max-h-[50px] text-lightPurple hover:text-lightBlue">
                 <Link href="https://www.linkedin.com/in/violetleon/">
                   <a target="_blank">
                     <FontAwesomeIcon className="" icon={faLinkedin} />
@@ -70,7 +74,7 @@ export default function Home() {
                 </Link>
                 <span>LinkedIn</span>
               </div>
-              <div className="m-10 max-w-[75px] max-h-[75px] text-lightPurple hover:text-lightBlue focus:ring-blue-300 dark:focus:ring-blue-800">
+              <div className="m-10 max-w-[50px] max-h-[50px] text-lightPurple hover:text-lightBlue focus:ring-blue-300 dark:focus:ring-blue-800">
                 <Link href="https://twitter.com/VioletteBunny">
                   <a target="_blank">
                     <FontAwesomeIcon className="" icon={faTwitter} />
