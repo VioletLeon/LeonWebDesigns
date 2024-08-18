ce0f5ff52a73e5e391ea16991f9e94a545f419d8
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-08-18 16:56:40 -0400

    google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,34 +2,36 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <title>Leon Web Designs</title>
-      <meta
-        name="description"
-        content="Leon Web Designs is a web design and development company that specializes in building custom websites for small businesses and entrepreneurs."
-      />
-      <meta
-        name="keywords"
-        content="web design, web development, custom websites, small businesses, entrepreneurs, custom software, software development, web design company, web development company"
-      />
-      <meta name="author" content="Leon Web Designs" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Leon Web Designs" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://leonwebdesigns.com" />
-      <meta
-        property="og:image"
-        content="https://leon-web-designs.s3.amazonaws.com/Logo.png"
-      />
-      <meta
-        property="og:description"
-        content="Leon Web Designs is a web design and development company that specializes in building custom websites for small businesses and entrepreneurs."
-      />
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <>
+      <Html lang="en">
+        <title>Leon Web Designs</title>
+        <meta
+          name="description"
+          content="Leon Web Designs is a web design and development company that specializes in building custom websites for small businesses and entrepreneurs."
+        />
+        <meta
+          name="keywords"
+          content="web design, web development, custom websites, small businesses, entrepreneurs, custom software, software development, web design company, web development company"
+        />
+        <meta name="author" content="Leon Web Designs" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Leon Web Designs" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://leonwebdesigns.com" />
+        <meta
+          property="og:image"
+          content="https://leon-web-designs.s3.amazonaws.com/Logo.png"
+        />
+        <meta
+          property="og:description"
+          content="Leon Web Designs is a web design and development company that specializes in building custom websites for small businesses and entrepreneurs."
+        />
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import LandingPage from '@/components/component/v0-software-consult';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main>
+      <GoogleTagManager gtmId="G-3HTPSW2YN7" />
       <LandingPage />
     </main>
   );
