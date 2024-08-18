6e1f192cf996487a65956bac7812b5908353cdd9
violettebunny <52981973+violettebunny@users.noreply.github.com>
2024-08-18 17:05:44 -0400

    google analytics ;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import LandingPage from '@/components/component/v0-software-consult';
 import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <GoogleTagManager gtmId="G-3HTPSW2YN7" />
+      <GoogleAnalytics gaId="G-3HTPSW2YN7 " />
       <LandingPage />
     </main>
   );
